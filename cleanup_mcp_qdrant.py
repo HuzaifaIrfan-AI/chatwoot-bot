2ed0eb854cad9a9f1bc0310c3e22853fac5905d0
HuzaifaIrfan <huzaifairfan2001@gmail.com>
2025-06-15 01:25:16 +0500

    cleanup mcp qdrant url

--- cleanup_mcp_qdrant.py
+++ cleanup_mcp_qdrant.py
@@ -6,7 +6,11 @@ load_dotenv(override=True)
 from qdrant_client import QdrantClient
 
 # client = QdrantClient(":memory:")  # Or use `host="localhost", port=6333`
-client = QdrantClient("http://localhost:6333")
+import os
+
+QDRANT_URL = os.getenv("QDRANT_URL", "http://localhost:6333")
+print(f"QDRANT_URL at '{QDRANT_URL}'")
+client = QdrantClient(QDRANT_URL)
 collection_name="mcp_middlehost"
 
 if client.collection_exists(collection_name=collection_name):
